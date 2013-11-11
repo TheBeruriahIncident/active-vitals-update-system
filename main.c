ba149fc1cebf6a378724c011fa719e881102f8f5
Adam Campbell <akroyvenslaka@gmail.com>
2013-11-11 02:19:19 -0500

    actually did the refactor in this version

--- main.c
+++ main.c
@@ -1,98 +1,168 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-//the size of the spatial decompositions
-#define	xSliceSize (5)
-#define	ySliceSize (5)
+#define PI	M_PI	/* pi to machine precision, defined in math.h */
+#define TWOPI	(2.0*PI)
+
+//how many data points to FFT
+#define bufferSize (14)
 
 //the size of the full image
 #define xImageSize (2592)
 #define yImageSize (1944)
 
+//the size of the spatial decompositions
+#define	xSliceSize (5)
+#define	ySliceSize (5)
+
+//number of slices
 #define xSlices (xImageSize / xSliceSize)
 #define ySlices (yImageSize / ySliceSize)
 
-//how many samples to keep
-#define bufferSize (14)
-#define frameRate (8)
+//TODO: figure the proper way to assign memory locations in order to get typing and everything
+//memory mapped IO locations
+#define cameraIO (0xDEADBEEF)
+#define serialIO (0xDEADBEEF)
 
-//memory mapped IO, TODO: set these
-#define imageIO 0xDEADBEEF
-#define serialIO 0xDEADBEF
+#define frameRate 8
 
 #define amplitudeThreshold (15)
 #define minimumHeartRate (60)
 #define maximumHeartRate (130)
-#define minimumBin (minimumHeartRate / 60 / frameRate * bufferSize)
-#define maximumBin (maximumHeartRate / 60 / frameRate * bufferSize)
+#define minimumBin ((int)((float)minimumHeartRate / 60 / frameRate * bufferSize + .5))
+#define maximumBin ((int)(float)maximumHeartRate / 60 / frameRate * bufferSize)
+
+/*
+ code from http://www-ee.uta.edu/eeweb/ip/Courses/DSP_new/Programs/fft.cpp
+ FFT/IFFT routine. (see pages 507-508 of Numerical Recipes in C)
+
+ Inputs:
+	data[] : array of complex* data points of size 2*NFFT+1.
+		data[0] is unused,
+		* the n'th complex number x(n), for 0 <= n <= length(x)-1, is stored as:
+			data[2*n+1] = real(x(n))
+			data[2*n+2] = imag(x(n))
+		if length(Nx) < NFFT, the remainder of the array must be padded with zeros
+
+	nn : FFT order NFFT. This MUST be a power of 2 and >= length(x).
+	isign:  if set to 1, 
+				computes the forward FFT
+			if set to -1, 
+				computes Inverse FFT - in this case the output values have
+				to be manually normalized by multiplying with 1/NFFT.
+ Outputs:
+	data[] : The FFT or IFFT results are stored in data, overwriting the input.
+*/
+
+void four1(double data[], int nn, int isign)
+{
+    int n, mmax, m, j, istep, i;
+    double wtemp, wr, wpr, wpi, wi, theta;
+    double tempr, tempi;
+    
+    n = nn << 1;
+    j = 1;
+    for (i = 1; i < n; i += 2) {
+	if (j > i) {
+	    tempr = data[j];     data[j] = data[i];     data[i] = tempr;
+	    tempr = data[j+1]; data[j+1] = data[i+1]; data[i+1] = tempr;
+	}
+	m = n >> 1;
+	while (m >= 2 && j > m) {
+	    j -= m;
+	    m >>= 1;
+	}
+	j += m;
+    }
+    mmax = 2;
+    while (n > mmax) {
+	istep = 2*mmax;
+	theta = TWOPI/(isign*mmax);
+	wtemp = sin(0.5*theta);
+	wpr = -2.0*wtemp*wtemp;
+	wpi = sin(theta);
+	wr = 1.0;
+	wi = 0.0;
+	for (m = 1; m < mmax; m += 2) {
+	    for (i = m; i <= n; i += istep) {
+		j =i + mmax;
+		tempr = wr*data[j]   - wi*data[j+1];
+		tempi = wr*data[j+1] + wi*data[j];
+		data[j]   = data[i]   - tempr;
+		data[j+1] = data[i+1] - tempi;
+		data[i] += tempr;
+		data[i+1] += tempi;
+	    }
+	    wr = (wtemp = wr)*wpr - wi*wpi + wr;
+	    wi = wi*wpr + wtemp*wpi + wi;
+	}
+	mmax = istep;
+    }
+}
+
+int readCoordinates(int x, int y)
+{
+	return cameraIO[y * xImageSize + x];
+}
 
 int main(int argc, char** argv)
 {
-	//the current frame in the wrapping buffer
-	int frameIndex = 0;
-	//wrapping buffer itself, initialized to all zeros
-	long samples[xSlices][ySlices][bufferSize] = {};
-	int freqs[xSlices][ySlices][bufferSize + 1] = {};
+	double samples[xSlices][ySlices][bufferSize];
 
-	//precompute the exponentials used in DFT
-	complex coefficients[bufferSize] = {};
-	for (int i = 0; i < bufferSize; ++i) {
-        double a = -2.0 * PI * i  / double(bufferSize);
-        coefficients[i] = complex(cos(a), sin(a));
-    }
-	
+	int NFFT = 2;
+	while(NFFT < bufferSize)
+	{
+		NFFT *= 2;
+	}
+
+	//run forever
 	while(true)
-	{	
-		//TODO: some sort of synchronization on frames
-	
-		for(int x = 0; x < xImageSize; x += xSliceSize)
+	{
+		//load in bufferSize data points
+		for(int frameIndex = 0; frameIndex < bufferSize; frameIndex++)
 		{
-			for(int y = 0; y < yImageSize; y += ySliceSize)
+			//TODO: synch with frames properly
+			
+			//iterate through all parts of the images
+			for(int x = 0; x < xImageSize; x += xSliceSize)
 			{
-				//sum the red over a spatial block
-				long total = 0;
-				for(int blockY = y; blockY < y + ySliceSize; blockY++)
+				for(int y = 0; y < yImageSize; y += ySliceSize)
 				{
-					for(int blockX = x; blockX < x + xSliceSize; blockX++)
+					//sum the intensity over a spatial block
+					long total = 0;
+					for(int blockY = y; blockY < y + ySliceSize; blockY++)
 					{
-						total += Red_Gain;
+						for(int blockX = x; blockX < x + xSliceSize; blockX++)
+						{
+							total += readCoordinates(blockX, blockY);
+						}
 					}
-				}
-				
-				//store the value
-				long previous = samples[x / xSliceSize][y / ySliceSize][frameIndex];
-				samples[x / xSliceSize][y / ySliceSize][frameIndex] = total;
-				
-				//compute the sliding DFT
-				total -= previous;
-				int ci = 0;
-				for (int i = 0; i < bufferSize; i++) {
-					freqs[i] += total * coefficients[x / xSliceSize][y / ySliceSize][ci];
-					if ((ci += frameIndex) >= bufferSize)
-						ci -= bufferSize;
+					
+					samples[x/xSliceSize][y/ySliceSize][frameIndex] = total;
 				}
 			}
 		}
 		
 		int strongestFreq = 0;
-		int strongestAmp = 0;
+		double strongestAmp = 0;
 		for(int x = 0; x < xSlices; x++)
 		{
 			for(int y = 0; y < ySlices; y++)
 			{
-				for(int i = minimumBin; i < maximumBin + 1; i++)
+				four1(samples[x][y], NFFT, 1);
+				
+				for(int i = minimumBin; i <= maximumBin; i++)
 				{
-					int amplitude = freqs[x][y][i];
-					if(amplitude > amplitudeThreshold && amplitude > strongestAmp)
+					double current = samples[x][y][i];
+					if(current > amplitudeThreshold && current > strongestAmp)
 					{
-						strongestFreq = (float)i /(float) bufferSize * frameRate * 60;
-						strongestAmp = amplitude;
+						strongestAmp = current;
+						strongestFreq = (double)i / bufferSize * frameRate * 60;
 					}
 				}
 			}
 		}
-		strongestFreq;//do whatever reporting we might want
 		
-		frameIndex = (frameIndex + 1) % bufferSize;
-	} 
+		*serialIO = strongestFreq;
+	}
 }
